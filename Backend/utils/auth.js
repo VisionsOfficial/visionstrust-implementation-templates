fecb2d3066bdea0781b9a5a7b446ebbef4d095f4
Felix Bole <67113170+Visions-Felix@users.noreply.github.com>
2021-07-15 16:52:11 +0200

    Added iat field to auth.js JWT signature

--- Backend/utils/auth.js
+++ Backend/utils/auth.js
@@ -2,6 +2,8 @@ module.exports = {
     /**
     * Generates a JWT for authentication with the Visionstrust API
     * 
+    * Expiration time is in minutes
+    *
     * @params {string} serviceKey The key of your service
     * @params {string} secretKey The secret key of your service
     */
@@ -9,6 +11,7 @@ module.exports = {
         let token = jwt.sign(
         {
             serviceKey: serviceKey,
+            iat: (new Date().getTime())
         },
         secretKey,
         { expiresIn: 5 * 60 }
